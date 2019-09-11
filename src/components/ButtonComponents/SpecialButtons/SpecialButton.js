4e2712637508aae23559608ef20bf9f9c7e68337
Josemaria <josemarianriagu25@gmail.com>
2019-09-12 03:18:47 +0800

    display received props from parent

--- src/components/ButtonComponents/SpecialButtons/SpecialButton.js
+++ src/components/ButtonComponents/SpecialButtons/SpecialButton.js
@@ -1,9 +1,13 @@
 import React from "react";
 
-const SpecialButton = () => {
+const SpecialButton = (props) => {
+  const { special } = props;
   return (
-    <>
+    <div className="special-button">
       {/* Display a button element rendering the data being passed down from the parent container on props */}
-    </>
+      {special}
+    </div>
   );
 };
+
+export default SpecialButton;
